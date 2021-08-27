d7891582869489f28b007ae2828116a92c4ccf3a
Ruben Quiroz Rangel <rqrsharingan@gmail.com>
2021-08-26 21:13:18 -0500

    VideoLikeCreator implementation

---
+++ src/Mooc/VideoLike/Application/Create/VideoLikeCreator.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace CodelyTv\Mooc\VideoLike\Application\Create;
+
+use CodelyTv\Mooc\Shared\Domain\Users\UserId;
+use CodelyTv\Mooc\Shared\Domain\Videos\VideoId;
+use CodelyTv\Mooc\VideoLike\Domain\VideoLike;
+use CodelyTv\Mooc\VideoLike\Domain\VideoLikeId;
+
+class VideoLikeCreator
+{
+   private VideoLikeRepository $videoLikeRepository;
+
+    public function __construct(VideoLikeRepository $videoLikeRepository)
+    {
+       $this->videoLikeRepository = $videoLikeRepository;
+    }
+
+    public function create(VideoLikeId $id,UserId $userId,VideoId $videoId){
+        $videoLike = VideoLike::like($id,$userId,$videoId);
+        $this->save($videoLike);
+    }
+}
---
+++ src/Mooc/VideoLike/Domain/VideoLike.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace CodelyTv\Mooc\VideoLike\Domain;
+
+class VideoLike
+{
+
+    public static function like(VideoLikeId $id, UserId $userId,VideoId $videoId)
+    {
+    }
+}
