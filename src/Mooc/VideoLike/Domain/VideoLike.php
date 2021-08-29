798269bd12c87d4d80f3271d303c2f5609ff24f4
Ruben Quiroz Rangel <rqrsharingan@gmail.com>
2021-08-29 13:01:36 -0500

    videoLike domain implementation

--- src/Mooc/VideoLike/Domain/VideoLike.php
+++ src/Mooc/VideoLike/Domain/VideoLike.php
@@ -4,11 +4,44 @@ namespace CodelyTv\Mooc\VideoLike\Domain;
 
 use CodelyTv\Mooc\Shared\Domain\Users\UserId;
 use CodelyTv\Mooc\Shared\Domain\Videos\VideoId;
+use CodelyTv\Shared\Domain\Aggregate\AggregateRoot;
 
-class VideoLike
+class VideoLike extends AggregateRoot
 {
-
-    public static function like(VideoLikeId $id, UserId $userId,VideoId $videoId)
+    public function __construct(private VideoLikeId $id, private UserId $userId, private VideoId $videoId)
     {
     }
+
+    public static function like(VideoLikeId $id, UserId $userId, VideoId $videoId): self
+    {
+        $videoLike = new self($id, $userId, $videoId);
+        $videoLike->record(new VideoLikeCreatedDomainEvent($id->getVideoLikeId(), $userId->getUserId(), $videoId->value()));
+        return $videoLike;
+    }
+
+    /**
+     * @return VideoLikeId
+     */
+    public function getId(): VideoLikeId
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return UserId
+     */
+    public function getUserId(): UserId
+    {
+        return $this->userId;
+    }
+
+    /**
+     * @return VideoId
+     */
+    public function getVideoId(): VideoId
+    {
+        return $this->videoId;
+    }
+
+
 }
